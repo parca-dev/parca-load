6816009f9068e7eafdebfecd65f15892ed4aec29
Matthias Loibl <mail@matthiasloibl.com>
2022-08-17 13:10:41 +0200

    Add custom histograms to all API calls
    Also run an internal HTTP server to expose Prometheus metrics

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/bufbuild/connect-go v0.1.1
+	github.com/prometheus/client_golang v1.12.2
 	github.com/matryer/is v1.4.0
 	github.com/prometheus/prometheus v0.36.2
 	go.buf.build/bufbuild/connect-go/parca-dev/parca v1.3.21
@@ -11,8 +12,15 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/grafana/regexp v0.0.0-20220304095617-2e8d9baf4ac2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.35.0 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	google.golang.org/genproto v0.0.0-20220711200234-e2551ce405a2 // indirect
 )
--- main.go
+++ main.go
@@ -13,6 +13,10 @@ import (
 	"time"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/prometheus/model/labels"
 	queryv1alpha1 "go.buf.build/bufbuild/connect-go/parca-dev/parca/parca/query/v1alpha1"
 	"go.buf.build/bufbuild/connect-go/parca-dev/parca/parca/query/v1alpha1/queryv1alpha1connect"
@@ -21,10 +25,13 @@ import (
 	"github.com/parca-dev/parca-load/sync"
 )
 
+const grpcCodeOK = "OK"
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	url := flag.String("url", "http://localhost:7070", "The URL for the Parca instance to query")
+	addr := flag.String("addr", "127.0.0.1:7171", "The address the HTTP server binds to")
 	flag.Parse()
 
 	client := queryv1alpha1connect.NewQueryServiceClient(
@@ -35,6 +42,10 @@ func main() {
 
 	ctx, stop := context.WithCancel(context.Background())
 	defer stop()
+
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(collectors.NewGoCollector())
+	reg.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
 
 	labels := sync.NewExpireMap[string, struct{}](time.Minute)
 	profileTypes := sync.NewExpireMap[string, struct{}](time.Minute)
@@ -47,14 +58,25 @@ func main() {
 		stop()
 	}()
 
-	go queryLabels(ctx, client, labels)
-	go queryValues(ctx, client, labels)
+	go queryLabels(ctx, client, reg, labels)
+	go queryValues(ctx, client, reg, labels)
 
-	go queryProfileTypes(ctx, client, profileTypes)
-	go queryQueryRange(ctx, client, profileTypes, series)
-	go queryQuerySingle(ctx, client, series)
+	go queryProfileTypes(ctx, client, reg, profileTypes)
+	go queryQueryRange(ctx, client, reg, profileTypes, series)
+	go queryQuerySingle(ctx, client, reg, series)
+
+	go internalServer(ctx, reg, *addr)
 
 	<-ctx.Done()
+}
+
+func internalServer(ctx context.Context, reg *prometheus.Registry, addr string) {
+	m := http.NewServeMux()
+	m.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+	log.Println("Running http server", addr)
+	if err := http.ListenAndServe(addr, m); err != nil {
+		log.Fatal(err)
+	}
 }
 
 type LabelStore interface {
@@ -65,8 +87,15 @@ type LabelStore interface {
 func queryLabels(
 	ctx context.Context,
 	client queryv1alpha1connect.QueryServiceClient,
+	reg *prometheus.Registry,
 	labelsStore LabelStore,
 ) {
+	histogram := promauto.With(reg).NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "parca_client_labels_seconds",
+		Help:    "The seconds it takes to make Labels requests against a Parca",
+		Buckets: []float64{0.025, 0.05, 0.075, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1},
+	}, []string{"grpc_code"})
+
 	ticker := time.NewTicker(5 * time.Second)
 	for {
 		select {
@@ -74,14 +103,15 @@ func queryLabels(
 			return
 		case <-ticker.C:
 			//log.Println("querying labels...")
-			//start := time.Now()
+			start := time.Now()
 			resp, err := client.Labels(ctx, connect.NewRequest(&queryv1alpha1.LabelsRequest{}))
 			if err != nil {
+				histogram.WithLabelValues(connect.CodeOf(err).String()).Observe(time.Since(start).Seconds())
 				log.Println("failed to make labels request", err)
 				continue
 			}
-			//log.Printf("querying labels took %v and got %d labels\n", time.Since(start), len(resp.Msg.LabelNames))
-
+			histogram.WithLabelValues(grpcCodeOK).Observe(time.Since(start).Seconds())
+			log.Printf("querying labels took %v and got %d labels\n", time.Since(start), len(resp.Msg.LabelNames))
 			for _, label := range resp.Msg.LabelNames {
 				labelsStore.Store(label, struct{}{})
 			}
@@ -92,8 +122,15 @@ func queryLabels(
 func queryValues(
 	ctx context.Context,
 	client queryv1alpha1connect.QueryServiceClient,
+	reg *prometheus.Registry,
 	labelsStore LabelStore,
 ) {
+	histogram := promauto.With(reg).NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "parca_client_values_seconds",
+		Help:    "The seconds it takes to make Values requests against a Parca",
+		Buckets: []float64{0.1, 0.2, 0.3, 0.4, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 1.25, 1.5, 1.75, 2, 3, 4, 5},
+	}, []string{"grpc_code"})
+
 	ticker := time.NewTicker(5 * time.Second)
 
 	for {
@@ -112,18 +149,21 @@ func queryValues(
 				continue
 			}
 
-			//start := time.Now()
+			start := time.Now()
 			_, err := client.Values(ctx, connect.NewRequest[queryv1alpha1.ValuesRequest](&queryv1alpha1.ValuesRequest{
 				LabelName: labelName,
 				Match:     nil,
 				Start:     timestamppb.New(time.Now().Add(-1 * time.Hour)),
 				End:       timestamppb.New(time.Now()),
 			}))
+
 			if err != nil {
+				histogram.WithLabelValues(connect.CodeOf(err).String()).Observe(time.Since(start).Seconds())
 				log.Println("failed to make values request", err)
 				continue
 			}
-			//log.Printf("querying values took %v for %s and got %d values\n", time.Since(start), labelName, len(resp.Msg.LabelValues))
+			histogram.WithLabelValues(grpcCodeOK).Observe(time.Since(start).Seconds())
+			log.Printf("querying values took %v for %s and got %d values\n", time.Since(start), labelName, len(resp.Msg.LabelValues))
 		}
 	}
 }
@@ -136,8 +176,15 @@ type ProfileTypeStore interface {
 func queryProfileTypes(
 	ctx context.Context,
 	client queryv1alpha1connect.QueryServiceClient,
+	reg *prometheus.Registry,
 	profileTypes ProfileTypeStore,
 ) {
+	histogram := promauto.With(reg).NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "parca_client_profiletypes_seconds",
+		Help:    "The seconds it takes to make ProfileTypes requests against a Parca",
+		Buckets: []float64{0.025, 0.05, 0.075, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 3, 4, 5},
+	}, []string{"grpc_code"})
+
 	ticker := time.NewTicker(10 * time.Second)
 	for {
 		select {
@@ -145,13 +192,15 @@ func queryProfileTypes(
 			return
 		case <-ticker.C:
 			//log.Println("querying profile types...")
-			//start := time.Now()
+			start := time.Now()
 			resp, err := client.ProfileTypes(ctx, connect.NewRequest[queryv1alpha1.ProfileTypesRequest](nil))
 			if err != nil {
+				histogram.WithLabelValues(connect.CodeOf(err).String()).Observe(time.Since(start).Seconds())
 				log.Println("failed to make profiles types request", err)
 				continue
 			}
-			//log.Printf("querying profile types took %v and got %d types\n", time.Since(start), len(resp.Msg.Types))
+			histogram.WithLabelValues(grpcCodeOK).Observe(time.Since(start).Seconds())
+			log.Printf("querying profile types took %v and got %d types\n", time.Since(start), len(resp.Msg.Types))
 
 			types := resp.Msg.GetTypes()
 			if len(types) == 0 {
@@ -176,9 +225,16 @@ type SeriesStore interface {
 func queryQueryRange(
 	ctx context.Context,
 	client queryv1alpha1connect.QueryServiceClient,
+	reg *prometheus.Registry,
 	profileTypesStore ProfileTypeStore,
 	seriesStore SeriesStore,
 ) {
+	histogram := promauto.With(reg).NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "parca_client_queryrange_seconds",
+		Help:    "The seconds it takes to make QueryRange requests against a Parca",
+		Buckets: []float64{0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.25, 1.5, 1.75, 2, 2.5, 3, 3.5, 4, 4.5, 5, 6, 7, 8, 9, 10},
+	}, []string{"grpc_code"})
+
 	ticker := time.NewTicker(15 * time.Second)
 
 	for {
@@ -197,17 +253,19 @@ func queryQueryRange(
 			}
 
 			//log.Println("querying query range...", pt)
-			//start := time.Now()
+			start := time.Now()
 			resp, err := client.QueryRange(ctx, connect.NewRequest[queryv1alpha1.QueryRangeRequest](&queryv1alpha1.QueryRangeRequest{
 				Query: pt,
 				Start: timestamppb.New(time.Now().Add(-1 * time.Hour)),
 				End:   timestamppb.New(time.Now()),
 			}))
 			if err != nil {
+				histogram.WithLabelValues(connect.CodeOf(err).String()).Observe(time.Since(start).Seconds())
 				log.Println(err)
 				continue
 			}
-			//log.Printf("querying range took %v for %s and got %d series\n", time.Since(start), pt, len(resp.Msg.Series))
+			histogram.WithLabelValues(grpcCodeOK).Observe(time.Since(start).Seconds())
+			log.Printf("querying range took %v for %s and got %d series\n", time.Since(start), pt, len(resp.Msg.Series))
 
 			for _, series := range resp.Msg.Series {
 				lset := labels.Labels{}
@@ -225,8 +283,16 @@ func queryQueryRange(
 func queryQuerySingle(
 	ctx context.Context,
 	client queryv1alpha1connect.QueryServiceClient,
+	reg *prometheus.Registry,
 	seriesStore SeriesStore,
 ) {
+	histogram := promauto.With(reg).NewHistogramVec(prometheus.HistogramOpts{
+		Name:        "parca_client_query_seconds",
+		Help:        "The seconds it takes to make Query requests against a Parca",
+		ConstLabels: map[string]string{"mode": "single"},
+		Buckets:     []float64{0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 3, 4, 5},
+	}, []string{"grpc_code"})
+
 	ticker := time.NewTicker(10 * time.Second)
 
 	for {
@@ -253,7 +319,7 @@ func queryQuerySingle(
 
 			//log.Println("querying single...", series, reportType.String(), sample.Timestamp.Seconds)
 
-			//start := time.Now()
+			start := time.Now()
 			_, err := client.Query(ctx, connect.NewRequest[queryv1alpha1.QueryRequest](&queryv1alpha1.QueryRequest{
 				Mode: queryv1alpha1.QueryRequest_MODE_SINGLE_UNSPECIFIED,
 				Options: &queryv1alpha1.QueryRequest_Single{
@@ -265,11 +331,13 @@ func queryQuerySingle(
 				ReportType: reportType,
 			}))
 			if err != nil {
+				histogram.WithLabelValues(connect.CodeOf(err).String()).Observe(time.Since(start).Seconds())
 				log.Println(err)
 				continue
 			}
 
-			//log.Printf("querying %s took %v for %s\n", reportType.String(), time.Since(start), series)
+			histogram.WithLabelValues(grpcCodeOK).Observe(time.Since(start).Seconds())
+			log.Printf("querying %s took %v for %s\n", reportType.String(), time.Since(start), series)
 		}
 	}
 }
